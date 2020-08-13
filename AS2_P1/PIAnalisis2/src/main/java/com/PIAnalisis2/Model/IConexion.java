428b61a518a666ff5fa30c2de7438d4ec5413eb0
Danielmced <danielmced@gmail.com>
2020-08-12 22:01:20 -0600

    He creado las interfaces pra asignaciones, cursos, conexion y estudiantes

--- AS2_P1/PIAnalisis2/src/main/java/com/PIAnalisis2/Model/IConexion.java
+++ AS2_P1/PIAnalisis2/src/main/java/com/PIAnalisis2/Model/IConexion.java
@@ -1,14 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.PIAnalisis2.Model;
 
-/**
- *
- * @author Danielmced
- */
 public interface IConexion {
-    
+        public void OpenConection();
+        public void CloseConection();
 }
