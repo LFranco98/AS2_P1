b3f14ca2fc07a98d262d817e6b78df80aa7adc2d
Danielmced <danielmced@gmail.com>
2020-08-12 21:48:38 -0600

    Se ha borrado la interfaz Iconexion de controladores y se ha creado IConexion en Controladores

--- AS2_P1/PIAnalisis2/src/main/java/com/PIAnalisis2/Model/IConexion.java
+++ AS2_P1/PIAnalisis2/src/main/java/com/PIAnalisis2/Model/IConexion.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.PIAnalisis2.Controller;
+package com.PIAnalisis2.Model;
 
 /**
  *
