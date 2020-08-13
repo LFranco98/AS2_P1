e5f4efbff07f1df4b38e53f84fb977a4f64b81ed
evelynzamora <56985019+evelynzamora@users.noreply.github.com>
2020-08-12 23:26:49 -0600

    Se agrego la clase Conexión

---
+++ PIAnalisis2/src/main/java/com/PIAnalisis2/Model/Conexion.java
@@ -0,0 +1,45 @@
+package com.PIAnalisis2.Model;
+
+
+public class Conexion {
+    private static Conexion instance;
+    public Connection conexion;
+    private final String urlConexion = "jdbc:mysql://192.185.4.65:3306/jbarilla_ingsoftware";
+    private final String jdbc = "com.mysql.cj.jdbc.Driver";
+    private final String usuario = "jbarilla_estudia";
+    private final String contra = "2Ui!OssHDQGv";
+    
+   
+    
+    private Conexion() {
+    }
+
+    public static Conexion getInstance(){
+        if(instance == null){
+            instance = new Conexion();
+        }
+        return instance;
+    }
+    
+    public void abrirConexion(){
+        try{
+            Class.forName(jdbc);
+            conexion = DriverManager.getConnection(urlConexion, usuario, contra);
+        } catch(ClassNotFoundException | SQLException ex){
+            
+        }
+    }
+    
+    public void cerrarConexion(){
+       try{
+            conexion.close();
+        } catch(Exception ex){
+             JOptionPane.showMessageDialog(null, ex.getMessage(), "Error en conetion",JOptionPane.ERROR_MESSAGE);
+        } 
+        
+    }
+    /**
+     * Default constructor
+     */
+    
+}
