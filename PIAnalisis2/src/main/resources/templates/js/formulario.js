072fa0a56d54bd73e36e25540abe8ba1e922deaf
Lester <lestertriforce@hotmail.com>
2020-08-12 22:01:24 -0600

    Se borro una carpeta erronea que se creo

--- PIAnalisis2/src/main/resources/templates/js/formulario.js
+++ PIAnalisis2/src/main/resources/templates/js/formulario.js
@@ -2,19 +2,26 @@ const formulario = document.getElementById('formulario');
 const inputs = document.querySelectorAll('#formulario input');
 
 const expresiones = {
-	usuario: /^[a-zA-Z0-9\_\-]{4,16}$/, // Letras, numeros, guion y guion_bajo
 	nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
-	password: /^.{4,12}$/, // 4 a 12 digitos.
+	apellido: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
 	correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
-	telefono: /^\d{7,14}$/ // 7 a 14 numeros.
+	birthday: /^\d{2}+\d{2}+\d{4}$/,
+	telefono1: /^[0-9_.+-]/, // 7 a 14 numeros.
+	telefono2: /^[0-9_.+-]/, // 7 a 14 numeros.
+	direccion: /^[a-zA-Z0-9_.+-]/,
+	direccion2: /^[a-zA-Z0-9_.+-]/,
 }
 
 const campos = {
-	usuario: false,
 	nombre: false,
-	password: false,
+	apellido: false,
 	correo: false,
-	telefono: false
+	birthday: false,
+	telefono1: false,
+	telefono2: false,
+	direccion1: false,
+	direccion2: false
+
 }
 
 const validarFormulario = (e) => {
@@ -59,26 +66,6 @@ const validarCampo = (expresion, input, campo) => {
 	}
 }
 
-const validarPassword2 = () => {
-	const inputPassword1 = document.getElementById('password');
-	const inputPassword2 = document.getElementById('password2');
-
-	if(inputPassword1.value !== inputPassword2.value){
-		document.getElementById(`grupo__password2`).classList.add('formulario__grupo-incorrecto');
-		document.getElementById(`grupo__password2`).classList.remove('formulario__grupo-correcto');
-		document.querySelector(`#grupo__password2 i`).classList.add('fa-times-circle');
-		document.querySelector(`#grupo__password2 i`).classList.remove('fa-check-circle');
-		document.querySelector(`#grupo__password2 .formulario__input-error`).classList.add('formulario__input-error-activo');
-		campos['password'] = false;
-	} else {
-		document.getElementById(`grupo__password2`).classList.remove('formulario__grupo-incorrecto');
-		document.getElementById(`grupo__password2`).classList.add('formulario__grupo-correcto');
-		document.querySelector(`#grupo__password2 i`).classList.remove('fa-times-circle');
-		document.querySelector(`#grupo__password2 i`).classList.add('fa-check-circle');
-		document.querySelector(`#grupo__password2 .formulario__input-error`).classList.remove('formulario__input-error-activo');
-		campos['password'] = true;
-	}
-}
 
 inputs.forEach((input) => {
 	input.addEventListener('keyup', validarFormulario);
